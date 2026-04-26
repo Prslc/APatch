05606fa30ac6639bebfe00f62884874693f5db02
Prslc <prslc113@gmail.com>
2026-04-27 03:43:04 +0800

    manager: guard checkMetaModuleWarning against missing root shell

--- app/src/main/java/me/bmax/apatch/ui/page/apm/APModuleViewModel.kt
+++ app/src/main/java/me/bmax/apatch/ui/page/apm/APModuleViewModel.kt
@@ -51,22 +51,23 @@ class APModuleViewModel : ViewModel() {
         uiState = uiState.copy(isNeedRefresh = true)
     }
 
-    private suspend fun checkMetaModuleWarning(modules: List<ModuleInfo>) = withContext(Dispatchers.IO) {
-        val needsMountModule = modules.any { module ->
-            val moduleDir = "/data/adb/modules/${module.id}"
-            // Module requires mounting if it has a system dir and no skip_mount file
-            SuFile.open("$moduleDir/system").isDirectory && !SuFile.open("$moduleDir/skip_mount").isFile
-        }
-
-        val warning = if (needsMountModule) {
-            val metaDir = "/data/adb/metamodule"
-            when {
-                !SuFile.open("$metaDir/module.prop").isFile -> apApp.getString(R.string.no_meta_module_installed)
-                SuFile.open("$metaDir/remove").isFile -> apApp.getString(R.string.meta_module_removed)
-                SuFile.open("$metaDir/disable").isFile -> apApp.getString(R.string.meta_module_disabled)
-                else -> null
+    private suspend fun checkMetaModuleWarning(modules: List<ModuleInfo>) {
+        val warning = runCatching {
+            val needsMountModule = modules.any { module ->
+                val moduleDir = "/data/adb/modules/${module.id}"
+                SuFile.open("$moduleDir/system").isDirectory && !SuFile.open("$moduleDir/skip_mount").isFile
             }
-        } else null
+
+            if (needsMountModule) {
+                val metaDir = "/data/adb/metamodule"
+                when {
+                    !SuFile.open("$metaDir/module.prop").isFile -> apApp.getString(R.string.no_meta_module_installed)
+                    SuFile.open("$metaDir/remove").isFile -> apApp.getString(R.string.meta_module_removed)
+                    SuFile.open("$metaDir/disable").isFile -> apApp.getString(R.string.meta_module_disabled)
+                    else -> null
+                }
+            } else null
+        }.getOrNull()
 
         withContext(Dispatchers.Main) {
             uiState = uiState.copy(metaModuleWarning = warning)
