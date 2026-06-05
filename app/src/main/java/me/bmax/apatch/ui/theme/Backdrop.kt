9d0d25dfd2698d2575e7d2eac8dd0a3b97f88d84
Prslc <prslc113@gmail.com>
2026-06-05 23:04:04 +0800

    manager: Bump miuix version to 0.9.2

--- app/src/main/java/me/bmax/apatch/ui/theme/Backdrop.kt
+++ app/src/main/java/me/bmax/apatch/ui/theme/Backdrop.kt
@@ -13,10 +13,10 @@ import androidx.compose.ui.graphics.Shape
 import top.yukonga.miuix.kmp.blur.BlendColorEntry
 import top.yukonga.miuix.kmp.blur.BlurColors
 import top.yukonga.miuix.kmp.blur.LayerBackdrop
-import top.yukonga.miuix.kmp.blur.isRenderEffectSupported
 import top.yukonga.miuix.kmp.blur.layerBackdrop
 import top.yukonga.miuix.kmp.blur.rememberLayerBackdrop
 import top.yukonga.miuix.kmp.blur.textureBlur
+import top.yukonga.miuix.kmp.shader.isRenderEffectSupported
 import top.yukonga.miuix.kmp.theme.MiuixTheme
 
 /**
